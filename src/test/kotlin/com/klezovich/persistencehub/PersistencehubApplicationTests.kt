726be0a8fef7cbe5e560e7a761cc51978f2add33
klezovich <arthur@trevor.io>
2022-09-25 14:31:08 +0200

    Initial commit

---
+++ src/test/kotlin/com/klezovich/persistencehub/PersistencehubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.klezovich.persistencehub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersistencehubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
